0aa84c1acbb754b328ede3812fe48bdbcd0760d3
Gustavo <gguimp@gmail.com>
2018-04-18 11:04:42 -0300

    Alteração para adicionar as funções de start e stop no mesmo arquivo python.

--- lambda_function.py
+++ lambda_function.py
@@ -47,12 +47,49 @@ def ec2start():
     else:
         LOGGER.info('Not started InstanceID: ' + INSTANCE_ID)
 
-def handler(event, context):
+def ec2stop():
     """
-    main function
+    EC2 instance stop
+    """
+    response = EC2CLIENT.describe_instance_status(InstanceIds=[INSTANCE_ID])
+    instance_status = response['InstanceStatuses'][0]['InstanceStatus']['Status']
+    system_status = response['InstanceStatuses'][0]['SystemStatus']['Status']
+
+    if instance_status == 'initializing' or system_status == 'initializing':
+        LOGGER.info('Aborts. Currently InstanceStatus Checking')
+        return
+    elif EC2INSTANCE.state['Name'] == 'running' and instance_status == 'ok' and system_status == 'ok':
+        EC2INSTANCE.stop()
+        LOGGER.info('Stop InstanceID： ' + INSTANCE_ID)
+        EC2CLIENT.get_waiter('instance_stopped').wait(
+            InstanceIds=[
+                INSTANCE_ID
+            ],
+            WaiterConfig={
+                'Delay': 5,  # Default: 15
+                'MaxAttempts': 30  # Default: 40
+            }
+        )
+        LOGGER.info("Completed!")
+        return
+    else:
+        LOGGER.info('Not Running InstanceID： ' + INSTANCE_ID)
+
+def start(event, context):
+    """
+    start function
     """
     try:
         ec2start()
+    except Exception as error:
+        LOGGER.exception(error)
+
+def stop(event, context):
+    """
+    stop function
+    """
+    try:
+        ec2stop()
     except Exception as error:
         LOGGER.exception(error)
 
